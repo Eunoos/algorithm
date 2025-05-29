6bdbc385ec568a09b5955e1761976b915067ab29
Eunoos <dmsdn18@gmail.com>
2025-05-29 15:33:05 +0900

    250529

---
+++ src/pg_피로도.java
@@ -0,0 +1,23 @@
+public class pg_피로도 {
+    class Solution {
+    int answer = 0;
+    boolean[] visited;
+
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        return answer;
+    }
+
+    public void dfs(int k, int[][] dungeons, int cnt) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;  //방문처리
+                dfs(k - dungeons[i][1], dungeons, cnt + 1);
+                visited[i] = false; //방문원래대로
+            }
+        }
+        answer = Math.max(answer, cnt); //최대값이면갱신
+    }
+}
+}
