fcbce48f222218c7eb8cf2b14431cccd3b29d3e6
Eunoos <dmsdn18@gmail.com>
2025-06-25 23:50:46 +0900

    250625

---
+++ src/pg_나머지가1되는수.java
@@ -0,0 +1,11 @@
+public class pg_나머지가1되는수 {
+    class Solution {
+    public int solution(int n) {
+        int answer=1;
+        while(n%answer!=1){
+            answer++;
+        }
+        return answer;
+    }
+}
+}
