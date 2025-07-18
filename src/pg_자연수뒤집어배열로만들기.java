a6c17a579e07c08bbb775dbc731db207acf249be
Eunoos <dmsdn18@gmail.com>
2025-07-18 15:44:43 +0900

    250718

---
+++ src/pg_자연수뒤집어배열로만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class pg_자연수뒤집어배열로만들기 {
+class Solution {
+    public int[] solution(long n) {
+        List<Integer> list = new ArrayList<>();
+        while(n>0){
+            list.add((int)(n%10));
+            n /= 10;
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
+}
