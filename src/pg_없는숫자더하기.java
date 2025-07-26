9d23429d08d8ac32c162c06aa249f28e5bfa4d3d
Eunoos <dmsdn18@gmail.com>
2025-07-26 23:23:39 +0900

    250726

---
+++ src/pg_없는숫자더하기.java
@@ -0,0 +1,11 @@
+public class pg_없는숫자더하기 {
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for(int n : numbers){
+            answer -= n;
+        }
+        return answer;
+    }
+}
+}
