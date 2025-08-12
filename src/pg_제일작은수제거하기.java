3e28cf66d0f06dd25db1e12bbd5853e9f8657e5a
Eunoos <dmsdn18@gmail.com>
2025-08-13 00:00:01 +0900

    250812

---
+++ src/pg_제일작은수제거하기.java
@@ -0,0 +1,20 @@
+public class pg_제일작은수제거하기 {
+    class Solution {
+    public int[] solution(int[] arr) {
+         if (arr.length == 1) return new int[]{-1};
+
+        int min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            if (num < min) min = num;
+        }
+
+        int[] answer = new int[arr.length - 1];
+        int idx = 0;
+        for (int num : arr) {
+            if (num != min) answer[idx++] = num;
+        }
+
+        return answer;
+    }
+}
+}
